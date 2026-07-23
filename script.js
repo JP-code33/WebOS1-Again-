87a580e8f147df3898769f2dac65f8433792ff44
JP-code33 <pateljhenil@gmail.com>
2026-07-23 09:11:44 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,58 @@
+
+dragElement(document.getElementById("Welcome"));
+
+
+function dragElement(element) {
+  
+  var initialX = 0;
+  var initialY = 0;
+  var currentX = 0;
+  var currentY = 0;
+
+  
+  if (document.getElementById(element.id + "welcomeheader")) {
+   
+    document.getElementById(element.id + "welcomeheader").onmousedown = startDragging;
+  } else {
+    
+    element.onmousedown = startDragging;
+  }
+
+  
+  function startDragging(e) {
+    e = e || window.event;
+    e.preventDefault();
+    
+    initialX = e.clientX;
+    initialY = e.clientY;
+    
+    document.onmouseup = stopDragging;
+    document.onmousemove = dragElement;
+  }
+
+
+  function dragElement(e) {
+    e = e || window.event;
+    e.preventDefault();
+  
+    currentX = initialX - e.clientX;
+    currentY = initialY - e.clientY;
+    initialX = e.clientX;
+    initialY = e.clientY;
+   
+    element.style.top = (element.offsetTop - currentY) + "px";
+    element.style.left = (element.offsetLeft - currentX) + "px";
+  }
+
+
+  function stopDragging() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+var welcomeScreen = document.querySelector("#welcome")
+
+function closeWindow(element) {
+    element.style.display = "flex"
+}
